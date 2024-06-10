04d79e52e714fb9fe0445aeac5dfe2c40105e9a1
VKU CODE <ungurean729@gmail.com>
2024-06-10 15:18:42 +0200

    google analytics integration

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleAnalytics trackingId="G-YLSK8Z9KJ3" />
       <body className={inter.className}>
         <Navbar />
         {children}
