b174ab4431ab1d73e1f4609572985bea82a3ff30
VKU CODE <ungurean729@gmail.com>
2024-06-19 15:23:02 +0200

    Update getdata.js

--- pages/api/getdata.js
+++ pages/api/getdata.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   try {
     console.log("Attempting to connect to the database...");
     const dbconnection = await mysql.createConnection({
-      host: "mysql.web-1.anybug.fr",
+      host: "194.164.72.35",
       database: "larbreapains",
       user: "larbreapains",
       password: "KODulFPwugHRiKO",
