71c80777bd89431accbe8a9d34b2cbb0669b0402
VKU CODE <ungurean729@gmail.com>
2024-06-19 15:04:01 +0200

    Update page.jsx

--- app/cuit/pain/page.jsx
+++ app/cuit/pain/page.jsx
@@ -10,7 +10,7 @@ export default function Pain() {
   useEffect(() => {
     async function getPageData() {
       try {
-        const apiUrlEndPoint = `https://larbreapains.fr/api/getdata`;
+        const apiUrlEndPoint = `https://larbreapains.fr/pages/api/getdata`;
         const response = await fetch(apiUrlEndPoint);
         if (!response.ok) {
           throw new Error('Network response was not ok');
