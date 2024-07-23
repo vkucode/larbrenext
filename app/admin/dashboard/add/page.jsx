ee6b1f496b2b7607071d77f3fc41c284550df4af
VKU CODE <ungurean729@gmail.com>
2024-07-23 16:11:00 +0300

    Update page.jsx

--- app/admin/dashboard/add/page.jsx
+++ app/admin/dashboard/add/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import styles from '../general.module.scss'
 
 export default function AddProduct() {
