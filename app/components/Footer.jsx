32b418e3958aea7406a5ade94a7560a48ace2f60
VKU CODE <ungurean729@gmail.com>
2024-08-15 13:13:12 +0200

    language 2.1.2

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -7,25 +7,25 @@ export default function Footer(){
     return(
         <>
             <section className={styles.footerSection}>
-                <Image src="/img/v2t.png" width={300} height={300} alt='شعار' />
+                <Image src="/img/v2t.png" width={300} height={300} alt='logo' />
                 <div className='flex flex-col lg:flex-row gap-20 justify-center items-start max-w-7xl mx-auto'>
                     <div className='flex flex-col justify-center items-center text-center'>
-                        <h2><FaMapMarkerAlt/>&nbsp;العنوان</h2>
-                        <p>102 شارع الشانزليزيه 75008 باريس</p>
+                        <h2><FaMapMarkerAlt/>&nbsp;Adresse</h2>
+                        <p>102 Av. des Champs-Élysées 75008 Paris</p>
                     </div>
                     <div className='flex flex-col justify-center items-center text-center'>
-                        <h2><MdOutlineAccessTime/>&nbsp;ساعات الطلبات</h2>
-                        <p>الإثنين-الأحد: 08:30 - 16:00</p>
+                        <h2><MdOutlineAccessTime/>&nbsp;Horaires de commandes</h2>
+                        <p>Lundi-Dimanche : 08:30 - 16:00</p>
                     </div>
                     <div className='flex flex-col justify-center items-center text-center'>
-                        <h2><FaMailBulk />معلومات إضافية</h2>
-                        <a href="tel:+33141192015">هاتف: +33 1 41 19 20 15</a>
-                        <a href="mailto: contactlarbreapains@gmail.com">البريد الإلكتروني: contactlarbreapains@gmail.com</a>
-                        <a href="/cgv">الشروط والأحكام: الشروط العامة للبيع</a>
+                        <h2><FaMailBulk />Information supplémentaire</h2>
+                        <a href="tel:+33141192015">Téléphone: +33 1 41 19 20 15</a>
+                        <a href="mailto: contactlarbreapains@gmail.com">Email : contactlarbreapains@gmail.com</a>
+                        <a href="/cgv">CGV: Conditions générales de ventes</a>
                     </div>
                 </div>
                 <div className='mt-10'>
-                    <p className='flex flex-row justify-center items-center'>©2024 شجرة الخبز. مشغل بواسطة <a href="https://vkucode.com"><Image src='/img/vkulogo.png' className='!w-[45px]' width={100} height={100} alt='vkucode' /></a></p>
+                    <p className='flex flex-row justify-center items-center'>©2024 L&apos;arbre à Pains. Powered by <a href="https://vkucode.com"><Image src='/img/vkulogo.png' className='!w-[45px]' width={100} height={100} alt='vkucode' /></a></p>
                 </div>
             </section>
         </>
