ca9cc8a83945e9781738bd718b9b8cdb7893d2e0
VKU CODE <ungurean729@gmail.com>
2024-10-25 15:12:32 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["larbreapains.fr", "res.cloudinary.com"], // Adăugăm res.cloudinary.com aici
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "larbreapains.fr",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
