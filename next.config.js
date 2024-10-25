a4b8e8534d86a2073386bfa93890ba1ac3667c23
VKU CODE <ungurean729@gmail.com>
2024-10-25 15:09:20 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["larbreapains.fr"],
+    domains: ["larbreapains.fr", "res.cloudinary.com"], // Adăugăm res.cloudinary.com aici
   },
 };
 
