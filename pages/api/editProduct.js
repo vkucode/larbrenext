7e3a265fd5746aa33e2627aa32cc34cd8665880a
VKU CODE <ungurean729@gmail.com>
2024-10-29 16:41:47 +0100

    Update editProduct.js

--- pages/api/editProduct.js
+++ pages/api/editProduct.js
@@ -32,18 +32,16 @@ export default async function handler(req, res) {
     return;
   }
 
-  let dbconnection;
+  if (req.method === "GET") {
+    let dbconnection;
+    try {
+      dbconnection = await mysql.createConnection({
+        host: "localhost",
+        database: "larbreapains",
+        user: "larbreapains",
+        password: "adminVku23#",
+      });
 
-  try {
-    // Inițializează conexiunea la baza de date
-    dbconnection = await mysql.createConnection({
-      host: "localhost",
-      database: "larbreapains",
-      user: "larbreapains",
-      password: "adminVku23#",
-    });
-
-    if (req.method === "GET") {
       const { id } = req.query;
       if (!id) {
         return res
@@ -58,25 +56,91 @@ export default async function handler(req, res) {
         return res.status(404).json({ message: "Produsul nu a fost găsit." });
       }
       res.status(200).json({ product: results[0] });
-    } else if (req.method === "PUT") {
-      console.log("Started PUT request");
-      const form = new formidable.IncomingForm({
-        maxFileSize: 10 * 1024 * 1024,
-      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    } finally {
+      if (dbconnection) await dbconnection.end();
+    }
+  } else if (req.method === "PUT") {
+    console.log("Started PUT request");
+    const form = new formidable.IncomingForm({ maxFileSize: 10 * 1024 * 1024 });
 
-      form.parse(req, async (err, fields, files) => {
-        if (err) {
-          console.error("File parsing error:", err);
-          return res
-            .status(500)
-            .json({ message: "Eroare la procesarea fișierelor." });
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error("File parsing error:", err);
+        return res
+          .status(500)
+          .json({ message: "Eroare la procesarea fișierelor." });
+      }
+
+      console.log("Fields parsed:", fields);
+      console.log("Files parsed:", files);
+
+      const {
+        id,
+        nume_ar,
+        nume_en,
+        nume,
+        descriere_ar,
+        descriere_en,
+        descriere,
+        tip,
+        categorie,
+      } = fields;
+
+      if (
+        !id ||
+        !nume_ar ||
+        !nume_en ||
+        !nume ||
+        !descriere_ar ||
+        !descriere_en ||
+        !descriere ||
+        !tip ||
+        !categorie
+      ) {
+        console.error("Missing fields in the request");
+        return res
+          .status(400)
+          .json({ message: "Toate câmpurile trebuie completate." });
+      }
+
+      let dbconnection;
+
+      try {
+        dbconnection = await mysql.createConnection({
+          host: "localhost",
+          database: "larbreapains",
+          user: "larbreapains",
+          password: "adminVku23#",
+        });
+
+        let imageUrl = null;
+        let ficheUrl = null;
+
+        // Încărcăm imaginea pe Google Cloud Storage dacă este furnizată
+        if (files.imagine) {
+          console.log("Image file provided:", files.imagine);
+          imageUrl = await uploadToGCS(
+            files.imagine.filepath,
+            `img/${Date.now()}_${files.imagine.originalFilename}`,
+            files.imagine.mimetype
+          );
         }
 
-        console.log("Fields parsed:", fields);
-        console.log("Files parsed:", files);
+        // Încărcăm fișierul tehnic pe Google Cloud Storage dacă este furnizat
+        if (files.fiche) {
+          console.log("Technical file provided:", files.fiche);
+          ficheUrl = await uploadToGCS(
+            files.fiche.filepath,
+            `fichetech/${Date.now()}_${files.fiche.originalFilename}`,
+            files.fiche.mimetype
+          );
+        }
 
-        const {
-          id,
+        // Construim și executăm interogarea SQL
+        let query = `UPDATE produits SET nume_produs_ar = ?, nume_produs_en = ?, nume_produs = ?, descriere_produs_ar = ?, descriere_produs_en = ?, descriere_produs = ?, tip_produs = ?, categoria_produs = ?`;
+        let queryParams = [
           nume_ar,
           nume_en,
           nume,
@@ -85,92 +149,33 @@ export default async function handler(req, res) {
           descriere,
           tip,
           categorie,
-        } = fields;
+        ];
 
-        if (
-          !id ||
-          !nume_ar ||
-          !nume_en ||
-          !nume ||
-          !descriere_ar ||
-          !descriere_en ||
-          !descriere ||
-          !tip ||
-          !categorie
-        ) {
-          console.error("Missing fields in the request");
-          return res
-            .status(400)
-            .json({ message: "Toate câmpurile trebuie completate." });
+        if (imageUrl) {
+          query += ", imagine_produs = ?";
+          queryParams.push(imageUrl);
         }
 
-        try {
-          let imageUrl = null;
-          let ficheUrl = null;
-
-          // Încărcăm imaginea pe Google Cloud Storage dacă este furnizată
-          if (files.imagine) {
-            console.log("Image file provided:", files.imagine);
-            imageUrl = await uploadToGCS(
-              files.imagine.filepath,
-              `img/${Date.now()}_${files.imagine.originalFilename}`,
-              files.imagine.mimetype
-            );
-          }
-
-          // Încărcăm fișierul tehnic pe Google Cloud Storage dacă este furnizat
-          if (files.fiche) {
-            console.log("Technical file provided:", files.fiche);
-            ficheUrl = await uploadToGCS(
-              files.fiche.filepath,
-              `fichetech/${Date.now()}_${files.fiche.originalFilename}`,
-              files.fiche.mimetype
-            );
-          }
-
-          // Construim și executăm interogarea SQL
-          let query = `UPDATE produits SET nume_produs_ar = ?, nume_produs_en = ?, nume_produs = ?, descriere_produs_ar = ?, descriere_produs_en = ?, descriere_produs = ?, tip_produs = ?, categoria_produs = ?`;
-          let queryParams = [
-            nume_ar,
-            nume_en,
-            nume,
-            descriere_ar,
-            descriere_en,
-            descriere,
-            tip,
-            categorie,
-          ];
-
-          if (imageUrl) {
-            query += ", imagine_produs = ?";
-            queryParams.push(imageUrl);
-          }
-
-          if (ficheUrl) {
-            query += ", fiche_tech = ?";
-            queryParams.push(ficheUrl);
-          }
-
-          query += " WHERE id = ?";
-          queryParams.push(id);
-
-          console.log("Executing SQL query with params:", queryParams);
-          await dbconnection.execute(query, queryParams);
-          res.status(200).json({ message: "Produs actualizat" });
-        } catch (error) {
-          console.error("Database update error:", error.message);
-          res.status(500).json({ message: error.message });
+        if (ficheUrl) {
+          query += ", fiche_tech = ?";
+          queryParams.push(ficheUrl);
         }
-      });
-    } else {
-      res.status(405).json({ message: "Metoda nu este permisă." });
-    }
-  } catch (error) {
-    console.error("Database connection error:", error.message);
-    res.status(500).json({ message: "Eroare la conectarea la baza de date" });
-  } finally {
-    // Închidem conexiunea după finalizarea cererii
-    if (dbconnection) await dbconnection.end();
+
+        query += " WHERE id = ?";
+        queryParams.push(id);
+
+        console.log("Executing SQL query with params:", queryParams);
+        await dbconnection.execute(query, queryParams);
+        res.status(200).json({ message: "Produs actualizat" });
+      } catch (error) {
+        console.error("Database update error:", error.message);
+        res.status(500).json({ message: error.message });
+      } finally {
+        if (dbconnection) await dbconnection.end();
+      }
+    });
+  } else {
+    res.status(405).json({ message: "Metoda nu este permisă." });
   }
 }
 
